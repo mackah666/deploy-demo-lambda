6279d464c090fc5acf4e5e99ed990edffa89e743
mackah666 <mackah666@yahoo.co.uk>
2021-10-01 09:50:52 +0100

    fix: made a minor change to the code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Mackah666 from Location Service'),
+        body: JSON.stringify('Hello Awaze from Location Service'),
     };
     return response;
 };
