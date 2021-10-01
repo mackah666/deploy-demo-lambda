098f288a627a2e16d5efa92db912d608115689a1
mackah666 <mackah666@yahoo.co.uk>
2021-10-01 11:08:55 +0100

    feat: updated msg

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Awaze from Manchester'),
+        body: JSON.stringify('Hello Awaze from Manchester, once again'),
     };
     return response;
 };
