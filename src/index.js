e0d98da31dd9a637e43d8c793aae22f1f357c656
mackah666 <mackah666@yahoo.co.uk>
2021-10-01 06:53:10 +0100

    fix: updated the lambda func

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello Mackah666 from Lambda!'),
+        body: JSON.stringify('Hello Mackah666 from Location Service'),
     };
     return response;
 };
