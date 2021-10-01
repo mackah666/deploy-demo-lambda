a20da56d70ef9e337f890f55470293851d09b3c6
mackah666 <mackah666@yahoo.co.uk>
2021-10-01 05:19:02 +0100

    feat: Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello Mackah666 from Lambda!'),
+    };
+    return response;
+};
